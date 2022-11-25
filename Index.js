b4c9804efcf98ba82da520f7a23db469a6d67866
Shagor <mdshagormahamud028@gmail.com>
2022-11-26 04:47:49 +0600

    app.get dashboard seller

--- Index.js
+++ Index.js
@@ -13,8 +13,24 @@ app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uuzniqz.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyingToken(req, res, next) {
+
+    const header = req.headers.authorization;
+    if (!header) {
+        return res.status(401).send('sorry unauthrize access');
+    }
+    const token = header.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
 
 
 async function run() {
@@ -40,11 +56,39 @@ async function run() {
             res.send(categories)
         })
         app.get('/allProducts', async (req, res) => {
-            const brand_name = req.query.category_name
-            console.log(brand_name);
+            // const email = req.query.email;
+            // const decodedEmail = req.decoded.email;
+            // if (email !== decodedEmail) {
+            //     return res.status(403).send({ message: 'forbidden access' });
+            // }
+            const brand_name = req.query.category_name;
             const query = { brand_name: brand_name }
             const category = await productsCollection.find(query).toArray()
             res.send(category)
+        })
+
+        // get all seller
+        app.get('/seller', verifyingToken, async (req, res) => {
+            const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            const query = { role: 'seller' }
+            const seller = await usersCollection.find(query).toArray()
+            res.send(seller)
+        })
+
+        // get all bayer 
+        app.get('/bayer', verifyingToken, async (req, res) => {
+            const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            const query = { role: 'bayer' }
+            const bayer = await usersCollection.find(query).toArray()
+            res.send(bayer)
         })
 
         //users post
@@ -53,6 +97,23 @@ async function run() {
             const result = await usersCollection.insertOne(users)
             res.send(result)
         })
+        //jwt token
+        app.post('/jwt', async (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: "2 days" })
+            res.send({ token })
+        })
+
+
+        //jwt token
+        // app.get('/jwt',async(req,res)=>{
+        //     const email=req.query.email;
+        //     const query={email:email}
+        //     const user=await usercollection.findOne(query)
+        //     res.send(user)
+        // })
+
+
         //bookings data
         // app.post('/bookings', async (req, res) => {
         //     const bookings = req.body;
